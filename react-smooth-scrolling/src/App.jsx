f00b419da6b5a3f8ba8d5fbb08da56b5189ec7e3
somy275 <somyranjank@gmail.com>
2024-09-23 15:02:02 +0530

    Smooth-scrolling-in-react

--- react-smooth-scrolling/src/App.jsx
+++ react-smooth-scrolling/src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Smooth1 from './Components/Smooth1'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Smooth2 from './Components/Smooth2'
+import Smooth3 from './Components/Smooth3'
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element:<><Smooth1/></>,
+    },
+    {
+path: '/Smooth2',
+element:<><Smooth2 /></>
+    },
+    {
+    path: '/Smooth3',
+element:<><Smooth3/></>
+    }
+  ])
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    {/* <Smooth1/> */}
+    <RouterProvider router={router} />
     </>
   )
 }
